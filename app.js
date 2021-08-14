4c3d0fc3e88384941d60bcf44be2657e6690f547
suwitna <suwitna@hotmail.com>
2021-08-14 15:05:24 +0700

    Update code
    Update code

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-app.get('/', (req, res) => res.send('Hello World'))
-app.get('/webhook', (req, res) => res.send('Hello World'))
-app.listen(port, () => {
-console.log(`Server is running on port : ${port}`)
-})
-
-module.exports = app
+app.post('/webhook', (req, res) => res.sendStatus(200))
+app.listen(port)
