b21c03bfe2e191277a002d3aa19809263d5752d8
suwitna <suwitna@hotmail.com>
2021-08-14 17:41:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 4000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+app.listen(port)
 /*
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
