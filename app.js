77dc5353676ceb7fc6ef88892a08e2f83745660b
suwitna <suwitna@hotmail.com>
2021-08-14 17:36:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+/*
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let user_type = req.body.events[0].source.type;
@@ -39,6 +40,51 @@ function reply(reply_token, msg) {
     .catch((err) => {
         // error handling
     });
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+*/
+
+app.post('/webhook',(req,res,next)=> { 
+    console.log("HTTP POST Request :: MikaTech Line App Webhook");
+    var reply_token = req.body.events[0].replyToken;
+    var user_type = req.body.events[0].source.type;
+    var user_id = req.body.events[0].source.userId
+    var msg = req.body.events[0].message.text;
+
+    console.log(`Message token : ${ reply_token }`);
+    console.log(`Message user Type : ${ user_type }`);
+    console.log(`Message user ID : ${ user_id }`);
+    console.log(`Message : ${ msg }`);
+    if (msg != null && msg != undefined){
+        console.log('Message :', msg.toString('utf8'));
+    }
+    reply(reply_token);
+    res.sendStatus(200);
+});
+
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {6P5TzfMs7eu/RHrY1vQzjU/Zn4+Z0BgN6vM7uNZN/ED/TWV0rReqn4GAzkEV64LNFvS3gXiEVSldCQZUZ76nQArk8mqqsLZYt2tDItvjaACADcNPEGm8jtZ5ZzbQUG2SLKirsfVJzpkj3Ak5B+P/ygdB04t89/1O/w1cDnyilFU=}'
+    }
+
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'สวัสดีคะ'
+        },
+        {
+            type: 'text',
+            text: 'ยินดีต้อนรับเข้าสู่ระบบสมาชิก MikaTech'
+        }]
+    })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
